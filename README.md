5ccd48cda54abba1dfc1e175491e9d694eb13305
Tzu-Chieh Chang <r05922176@ntu.edu.tw>
2024-04-06 00:16:20 +0800

    add more graphs

--- README.md
+++ README.md
@@ -4,9 +4,17 @@ Data for the 0403 earthquake that happened on 04/03/2024 in Taiwan.
 
 ## Visualizations
 
-Earthquake magnitudes (ML) over time:
+Earthquake magnitudes (ML) over time (red: ML >= 5.5, green: 4 <= ML < 5.5, blue: ML < 4):
 
 ![magnitudes over time](images/mag_t.png)
+
+Earthquake frequency (running average over 4 hours):
+
+![counts over time](images/count_t.png)
+
+Earthquake cumulative energy (ML equivalent):
+
+![cumulative energy](images/energy_t.png)
 
 Spatial distribution of the epicenters:
 
--- graph.py
+++ graph.py
@@ -1,11 +1,19 @@
 import csv
 from pathlib import Path
-from datetime import datetime
+from datetime import datetime, timedelta
 from PIL import Image
 
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+
+
+def magnitude_to_energy(magnitude):
+    return 10.0 ** (4.8 + 1.5 * magnitude)
+
+
+def energy_to_magnitude(energy):
+    return (np.log10(energy) - 4.8) / 1.5
 
 
 class EarthquakeData:
@@ -14,9 +22,32 @@ class EarthquakeData:
         self.times = []
         self.longitudes = []
         self.latitudes = []
-        self.magitudes = []
+        self.magnitudes = []
         self.depths = []
         self.location_descriptions = []
+
+    def process(self):
+        # Calculate running average of earthquake counts
+        avg_hours = 4.0
+        half_hours = timedelta(hours=avg_hours / 2)
+        self.counts = []
+        for time in self.times:
+            self.counts.append(self.count(time - half_hours, time + half_hours) / avg_hours)
+
+        # Calculate cumulative energy release amount
+        total_energy = 0.0
+        self.cumulative_magnitudes = []
+        for magnitude in reversed(self.magnitudes):
+            total_energy += magnitude_to_energy(magnitude)
+            self.cumulative_magnitudes.insert(0, energy_to_magnitude(total_energy))
+
+    def count(self, begin_time, end_time):
+        count = 0
+        for time in self.times:
+            if time >= begin_time and time < end_time:
+                count += 1
+        return count
+
 
 def read_data(file_name, begin_time, end_time):
     time_format = '%Y-%m-%d %H:%M:%S'
@@ -40,13 +71,13 @@ def read_data(file_name, begin_time, end_time):
             data.times.append(time)
             data.longitudes.append(float(row[2]))
             data.latitudes.append(float(row[3]))
-            data.magitudes.append(float(row[4]))
+            data.magnitudes.append(float(row[4]))
             data.depths.append(float(row[5]))
             data.location_descriptions.append(row[6])
 
     data.longitudes = np.array(data.longitudes)
     data.latitudes = np.array(data.latitudes)
-    data.magitudes = np.array(data.magitudes)
+    data.magnitudes = np.array(data.magnitudes)
     data.depths = np.array(data.depths)
 
     return data
@@ -59,17 +90,39 @@ matplotlib.use('Agg')
 Path("./outputs/").mkdir(parents=True, exist_ok=True)
 
 data = read_data("data/20240405_0742.csv", "2024-04-03 07:58:00", "2025-04-03 07:58:00")
+data.process()
 
 fig = plt.figure(figsize=(12, 5))
 ax = fig.add_subplot()
 ax.bar(
     data.times, 
-    data.magitudes,
+    data.magnitudes,
     width=0.0075,
-    alpha=0.5)
+    alpha=0.5,
+    color=np.where(data.magnitudes >= 5.5, 'r', np.where(data.magnitudes < 4, 'b', 'g')))
 ax.set_xlabel("Time")
 ax.set_ylabel("Magnitude")
 plt.savefig(Path("./outputs/mag_t.png"))
+plt.clf()
+
+fig = plt.figure(figsize=(12, 5))
+ax = fig.add_subplot()
+ax.plot(
+    data.times, 
+    data.counts)
+ax.set_xlabel("Time")
+ax.set_ylabel("Counts (4 hours mean)")
+plt.savefig(Path("./outputs/count_t.png"))
+plt.clf()
+
+fig = plt.figure(figsize=(12, 5))
+ax = fig.add_subplot()
+ax.plot(
+    data.times, 
+    data.cumulative_magnitudes)
+ax.set_xlabel("Time")
+ax.set_ylabel("Cumulative Energy")
+plt.savefig(Path("./outputs/energy_t.png"))
 plt.clf()
 
 fig = plt.figure(figsize=(12, 12))
@@ -79,8 +132,8 @@ ax.scatter(
     data.latitudes,
     data.depths,
     c=data.depths,
-    s=data.magitudes ** 3.5,
-    alpha=0.7,
+    s=data.magnitudes ** 3.8,
+    alpha=0.6,
     edgecolors='gray',
     cmap='viridis_r')
 ax.set_xlabel("Longitude")
